acce9f18195baec49c1afd82f45188fafda94322
ChritoferGa <christofergallo1@gmail.com>
2021-06-04 21:24:43 -0500

    Instalacion de rimraf

---
+++ dist-server/public/javascripts/TercerEjercicio.js
@@ -0,0 +1,3 @@
+"use strict";
+
+alert('Tercer Ejercicio');
---
+++ dist-server/routes/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    author: 'Programacion web fullstack'
+  });
+});
+/* Primer Ejercicio */
+
+router.get('/PrimerEjercicio', function (req, res, next) {
+  res.send('Bienvenido al curso de programación web fullstack este es el primer ejercicio');
+});
+/* Segundo Ejercicio */
+
+router.get('/SegundoEjercicio', function (req, res, next) {
+  res.status(200).json({
+    message: 'Es el Segundo ejercicio'
+  });
+});
+module.exports = router;
