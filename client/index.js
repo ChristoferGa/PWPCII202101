2b22dfe1707ea80024593f7af8561c3f4b24b8f1
ChritoferGa <christofergallo1@gmail.com>
2021-06-05 18:43:11 -0500

    Sesión 28

--- client/index.js
+++ client/index.js
@@ -1,28 +1,2 @@
-/* eslint-disable no-console */
-/* eslint-disable no-alert */
 import './stylesheets/style.css';
 import './stylesheets/mystyle.css';
-
-console.log('Webpack Working!!!');
-// Default parameters solo disponible en ES6
-const show = (m = 'Hola') => {
-  alert(m);
-};
-
-show();
-
-function resolverAfter2Seconds() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
-}
-
-async function asyncCall() {
-  console.log('Call an async function');
-  const result = await resolverAfter2Seconds();
-  console.log(result);
-}
-
-asyncCall();
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Programacion web fullstack' });
+  res.render('index', { title: 'Express', author: 'Programacion web fullstack', babel: 'Babel funcionando'});
 });
 
 /* Primer Ejercicio */
