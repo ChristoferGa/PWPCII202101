cf9c055e19f30c4a280319c504fb480df0962b6e
ChritoferGa <christofergallo1@gmail.com>
2021-06-04 22:29:59 -0500

    Integrando babel a Webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,14 +7,52 @@ module.exports = {
     // 3. Especificando el archivo de salida
     output: {
         // 3. Ruta absoluta de salida
-        path: path.join(__dirname, 'public') ,
+        path: path.join(__dirname, 'public'),
         // 5. Nombre del archivo de salida
-        filename: 'js/bundle.js'
+        filename: 'js/bundle.js',
         // 6. Ruta del path publica para fines del servidor de desarrollo
+        publicPath: '/'
     },
     devServer: {
-        contentBase: './public',
+        contentBase: path.join(__dirname, 'public'),
         port: 8084,
         host: 'localhost'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        'modules': false,
+                                        'useBuiltIns': 'usage',
+                                        'targets': {"chrome": "80"},
+                                        'corejs': 3
+                                    }
+                                ]
+                            ],
+                            "plugins": [
+                                [
+                                    "module-resolver",
+                                    {
+                                        "root": ["./"],
+                                        "alias": {
+                                            "@client": "./client",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
